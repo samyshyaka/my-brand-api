c3cd238015887135aaa07a0db71658a0c5a93187
samuel shyaka dushimimana <samyshyaka@gmail.com>
2022-01-13 23:24:31 +0200

    documented the articles route with swagger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import index from './routes/index.js';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const url = "mongodb://localhost/ArticlesDBex";
 
@@ -10,6 +12,24 @@ app.use(express.json());
 mongoose.connect(url);
 const con = mongoose.connection;
 
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      version: "1.0.0",
+      title: "My Brand API",
+      description: "Personal Portfolio",
+      contact: {
+        name: "Samuel Shyaka Dushimimana"
+      },
+      servers: ["http://localhost:9000/articles"]
+    }
+  },
+  apis: ["./src/routes/articles.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 con.on("connected", () => {
   console.log("connected..");
--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,3 +1,24 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Article:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The article's title.
+ *           example: Benefits of programming
+ *         author:
+ *           type: string
+ *           description: Author of the article.
+ *           example: Sam
+ *         content:
+ *           type: string
+ *           description: Content of the article.
+ *           example: Programming improves critical thinking.
+ */
+
 import express from 'express';
 import { authenticateToken } from '../middleware/verifyJWT.js'
 import { 
@@ -12,21 +33,141 @@ const router = express.Router()
 
 //Get Method
 
+/**
+ * @swagger
+ * /articles:
+ *   get:
+ *     description: Retrieve a list of Articles. * 
+ *     responses:
+ *       200:
+ *         description: A list of articles.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Article'
+ */
+
 router.get('/', getArticlesHandler)
 
-//Get Method - Display one single object
+//Get a single object
 
+/**
+ * @swagger
+ * /articles/{id}:
+ *   get:
+ *     description: Retrieve a single article.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the article to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A single article.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: '#/components/schemas/Article'
+ */
 router.get('/:id', getSpecificArticleHandler)
 
-//Post Method
+//Post an article
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     description: Create a JSONPlaceholder user.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               $ref: '#/components/schemas/Article'
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: '#/components/schemas/Article'
+*/
 
 router.post('/', authenticateToken, postArticleHandler)
 
 //Patch Method
 
+/**
+ * @swagger
+ * /articles/{id}:
+ *   patch:
+ *     description: Edit an single article.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the article to retrieve.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Article'
+ *       200:
+ *         description: Edit an article.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: '#/components/schemas/Article'
+*/
+
 router.patch("/:id", authenticateToken, patchArticleHandler)
 
-// Delete Method
+// Delete an article
+
+/**
+ * @swagger
+ * /articles/{id}:
+ *   delete:
+ *     description: Delete an article.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the article to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Delete one article.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: '#/components/schemas/Article'
+*/
 
 router.delete("/:id", authenticateToken, deleteArticleHandler)
 
