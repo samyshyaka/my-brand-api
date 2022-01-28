b7b63d113d33542f827e905f2afc6a0c8e0100b9
samuel shyaka dushimimana <samyshyaka@gmail.com>
2022-01-28 09:19:50 +0200

    updated the cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 import { readFile } from "fs/promises";
 
@@ -20,6 +21,8 @@ const swaggerDocument  = JSON.parse(await readFile("./swagger.json"));
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(
   '/api-docs',
