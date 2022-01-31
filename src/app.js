d918e04a68bd9f4f2e31319f7adea44594db6787
samuel shyaka dushimimana <samyshyaka@gmail.com>
2022-01-31 13:48:44 +0200

    enabled cors

--- src/app.js
+++ src/app.js
@@ -19,13 +19,9 @@ const swaggerDocument  = JSON.parse(await readFile("./swagger.json"));
 
 const app = express();
 
-app.use(express.json());
+app.use(cors())
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(express.json());
 
 app.use(
   '/api-docs',
