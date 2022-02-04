412595b74ff02b5d2ae50772cbfb447817f4190b
samuel-shyaka-dus <samyshyaka@gmail.com>
2022-02-04 15:32:00 +0200

    updated commentController.js

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -17,9 +17,8 @@ const getCommentsHandler = async(req,res) => {
 }
 
 const getSpecificCommentHandler = async(req,res) => {
-
-    const comment = await Comment.findById(req.params.id)
-    console.log(comment);
+    try {
+        const comment = await Comment.findById(req.params.id)
         if (!comment) {
             return res.status(404).send({
                 status : "fail",
@@ -27,12 +26,6 @@ const getSpecificCommentHandler = async(req,res) => {
                 message : "comment not found"
             })
         }
-        
-         
-    try {
-        
-
-        
         return res.status(200).json({
             status : "success",
             code: 200,
@@ -44,7 +37,7 @@ const getSpecificCommentHandler = async(req,res) => {
         return res.status(500).send({
             status : "error",
             code: 500,
-            message : err
+            message : "unable to communicate with the database"
         })
     }
 }
